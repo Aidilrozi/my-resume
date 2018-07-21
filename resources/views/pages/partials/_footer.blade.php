3f1a33f67180f60d587881c8bdc3ee5b646da597
mohd.aidilrozi <mohd.aidilrozi@gmail.com>
2018-07-22 02:54:42 +0800

    app first commit

---
+++ resources/views/pages/partials/_footer.blade.php
@@ -0,0 +1,9 @@
+ <!-- Bootstrap core JavaScript -->
+ <script src="{{ asset('jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{ asset('jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{ asset('resume_styling/js/resume.min.js') }}"></script>
